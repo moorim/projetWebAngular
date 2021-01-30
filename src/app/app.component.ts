7e81bfc40d4c3863792a1345d5648980686b5d8c
moorim <rchebbi94@gmail.com>
2021-01-30 11:52:25 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'training-app';
+  nbpart: number=0;
+  firstSession= {
+    id: 1,
+    name: 'Formation Angular',
+    track: 'MEAN Stack',
+    date: new Date('2020-11-19'),
+    duree: 3,
+    local: 'Tunis',
+    participants: 0
+  }
+  nbrParticipantsChange (event)
+  {
+   this.nbpart = event.value;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SessionComponent } from './session/session.component';
+import { SessionItelListComponent } from './session-itel-list/session-itel-list.component';
+import { InscriptionDisabledDirectiveDirective } from './inscription-disabled-directive.directive';
 
+import {RouterModule, Routes} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+const appRoutes: Routes = [{
+  path: 'list',component: SessionItelListComponent},
+  {path: 'admin',loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule)},
+  { path: '',   redirectTo: '/list', pathMatch: 'full' }
+];
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [  
+    AppComponent,
+    SessionComponent,
+    SessionItelListComponent,
+    InscriptionDisabledDirectiveDirective,
+   
   ],
-  imports: [
+  imports: [RouterModule.forRoot(appRoutes,{ enableTracing: true }),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
